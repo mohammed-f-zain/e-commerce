cb38b0be7fb12f3f322a321121b3685746b9acd2
Mohammad-Abdelhadi <mohammad.b.abdelhadi@gmail.com>
2023-08-25 01:31:56 +0300

    edit the item page

--- view/e-commerce/pages/Items.js
+++ view/e-commerce/pages/Items.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { View, Text, Image, StatusBar, ScrollView, TouchableOpacity } from 'react-native';
-import { StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 function Items({ route }) {
   const { itemData } = route.params;
@@ -10,27 +17,27 @@ function Items({ route }) {
   navigation.setOptions({
     title: itemData.name,
     headerStyle: {
-      backgroundColor: '#EFEFF2',
+      backgroundColor: "#EFEFF2",
     },
-    headerTintColor: '#000',
+    headerTintColor: "#000",
   });
 
   return (
     <View style={styles.container}>
       <ScrollView>
         <StatusBar backgroundColor="#EFEFF2" barStyle="dark-content" />
-       
-      <Image
-        src={itemData.image}
-        // resizeMode="center"
-        style={{
-          width: 130,
-          height: 140,
-          marginBottom: 3,
-          backgroundColor: "#FFFAF6",
-          borderRadius: 8,
-        }}
-      />
+
+        <Image
+          src={itemData.image}
+          // resizeMode="center"
+          style={{
+            width: 410,
+            height: 590,
+            marginBottom: 3,
+            backgroundColor: "#FFFAF6",
+            borderRadius: 8,
+          }}
+        />
         <View style={styles.details}>
           <View style={styles.headerItem}>
             <Text style={styles.itemName}>{itemData.name}</Text>
@@ -40,7 +47,10 @@ function Items({ route }) {
             <Text style={styles.description}>{itemData.description}</Text>
             <TouchableOpacity
               style={styles.button}
-              onPress={() => {/* Handle Add to Cart */}}>
+              onPress={() => {
+                /* Handle Add to Cart */
+              }}
+            >
               <Text style={styles.buttonText}>Add to cart</Text>
             </TouchableOpacity>
           </View>
@@ -49,59 +59,60 @@ function Items({ route }) {
     </View>
   );
 }
+
+export default Items;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection:"column",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
     justifyContent: "center",
-    alignItems: 'center',
   },
-  details:{
-    backgroundColor:"#fff",
-    borderTopRightRadius:50,
-    padding:20,
-    borderTopLeftRadius: 50,
-    },
-    headerItem:{
-    flex:1,
-    flexDirection:"row",
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom:40,
-    },
-  itemName:{
+  // test: {
+  //   flex: 1,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   width: 300,
+  //   height: 400,
+  // },
+  details: {
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  headerItem: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 40,
+  },
+  itemName: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  button:{
-  backgroundColor:"#B7076B",
-  borderRadius:10,
-  textAlign:'center',
-  padding:10,
-  marginTop:15,
+  button: {
+    backgroundColor: "#B7076B",
+    borderRadius: 10,
+    textAlign: "center",
+    padding: 10,
+    marginTop: 15,
   },
-  buttonText:{
-  color: 'white',
-  fontSize: 18,
-  fontWeight: 'bold',
-  textAlign:'center',
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
   },
-  description:{
-  color:"#7F7F7F",
-  lineHeight:22,
-  fontSize:15
+  description: {
+    color: "#7F7F7F",
+    lineHeight: 22,
+    fontSize: 15,
   },
-  descBtn:{
-  flex:1,
-  flexDirection:"column",
-  justifyContent: "space-between",
-  gap:19,
-  }
-  });
-  
-  export default Items;
+  descBtn: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    gap: 19,
+  },
+});
