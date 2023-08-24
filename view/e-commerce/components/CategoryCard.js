ac851d9dfcdb69dc0a3382a58c014f2a5ec62ddc
Ehab-Aqili <ihabaloqaily@gmail.com>
2023-08-25 00:39:36 +0300

    get product and categories and search input

--- view/e-commerce/components/CategoryCard.js
+++ view/e-commerce/components/CategoryCard.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet, Image } from 'react-native';
 
 const CategoryCard = ({name , image , isSelected}) => {
   return (
-    //  A component to render individual item
+   
         <View style={[
           styles.item,
           {
@@ -19,38 +19,6 @@ const CategoryCard = ({name , image , isSelected}) => {
           />
         <Text style={{color: 'black', textAlign: 'center', marginBottom:5,}}>{name}</Text>
         </View>
-        
-    // <TouchableOpacity
-    //   style={styles.container(selectedJob, item)}
-    //   onPress={() => handleCardPress(item)}
-    // >
-    //   <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
-    //     <Image
-    //       source={{
-    //         uri: checkImageURL(item?.employer_logo)
-    //           ? item.employer_logo
-    //           : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
-    //       }}
-    //       resizeMode='contain'
-    //       style={styles.logoImage}
-    //     />
-    //   </TouchableOpacity>
-    //   <Text style={styles.companyName} numberOfLines={1}>
-    //     {item.employer_name}
-    //   </Text>
-
-    //   <View style={styles.infoContainer}>
-    //     <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
-    //       {item.job_title}
-    //     </Text>
-    //     <View style={styles.infoWrapper}>
-    //       <Text style={styles.publisher(selectedJob, item)}>
-    //         {item?.job_publisher} -
-    //       </Text>
-    //       <Text style={styles.location}> {item.job_country}</Text>
-    //     </View>
-    //   </View>
-    // </TouchableOpacity>
   )
 }
 const styles = StyleSheet.create({
@@ -63,10 +31,6 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         height: 100,
         width: 100,
-        // shadowColor: '#000',
-        // shadowOffset: { width: 3, height: 3 },
-        // shadowOpacity: 0.3,
-        // shadowRadius: 8,
         },
 })
 export default CategoryCard
--- view/e-commerce/pages/Home.js
+++ view/e-commerce/pages/Home.js
@@ -1,7 +1,6 @@
-import React, { useState , useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
 import styles from "./style";
-import axios from "axios"
+import axios from "axios";
 import {
   Alert,
   Keyboard,
@@ -24,31 +23,99 @@ import { useNavigation } from "@react-navigation/native";
 const Home = () => {
   const navigation = useNavigation();
   const [searchText, setSearchText] = useState("");
+  const [selectedCategoryName, setSelectedCategoryName] = useState(null);
 
-  const [itemData,setItemData]=useState([]);
-useEffect(() => {
-    axios.get("https://project-e-commerce-v4bs.onrender.com/products")
-      .then(response => {
+  const [itemData, setItemData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://project-e-commerce-v4bs.onrender.com/products")
+      .then((response) => {
         setItemData(response.data);
-        
+        setFilteredData(response.data); // Initialize filtered data with all products
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }, []);
-  // console.log(itemData.splice(0,2))
- 
+
+  useEffect(() => {
+    if (selectedCategoryName) {
+      // Filter products based on selected category
+      const filteredProducts = itemData.filter(
+        (item) => item.name === selectedCategoryName
+      );
+      setFilteredData(filteredProducts);
+    } else {
+      // If no category is selected, show all products
+      setFilteredData(itemData);
+    }
+  }, [selectedCategoryName, itemData]);
+  const cat = [
+    {
+      CategoryName: "T-shirt",
+      image: "https://cdn.icon-icons.com/icons2/1082/PNG/512/tshirt_78128.png",
+      isSelected: false,
+    },
+    {
+      CategoryName: "Jacket",
+      image: "https://cdn-icons-png.flaticon.com/512/1785/1785396.png",
+      isSelected: false,
+    },
+    {
+      CategoryName: "Jeans",
+      image: "https://cdn-icons-png.flaticon.com/512/3893/3893044.png",
+      isSelected: false,
+    },
+  ];
+  useEffect(() => {
+    if (searchText) {
+      // Filter products based on search text
+      const filteredProducts = itemData.filter((item) =>
+        item.name.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setFilteredData(filteredProducts);
+    } else {
+      // If search text is empty, show all products or filtered by category
+      if (selectedCategoryName) {
+        const filteredProducts = itemData.filter(
+          (item) => item.category === selectedCategoryName
+        );
+        setFilteredData(filteredProducts);
+      } else {
+        setFilteredData(itemData);
+      }
+    }
+  }, [searchText, itemData, selectedCategoryName]);
+
   const handleSearch = (text) => {
     setSearchText(text);
   };
 
+  const OnCategoryPress = (categoryName) => {
+    if (selectedCategoryName === categoryName) {
+      setSelectedCategoryName(null);
+    } else {
+      setSelectedCategoryName(categoryName);
+    }
+  };
 
- 
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => OnCategoryPress(item.CategoryName)}>
+      <CategoryCard
+        name={item.CategoryName}
+        image={item.image}
+        isSelected={item.CategoryName === selectedCategoryName}
+      />
+    </TouchableOpacity>
+  );
 
-  
+  const OnProductPress = (itemId) => {
+    navigation.navigate("Item", { itemId });
+  };
 
   return (
-    
     <View
       style={{
         flex: 1,
@@ -70,26 +137,31 @@ useEffect(() => {
           onChangeText={handleSearch}
         />
       </View>
-      {/* select -> pink , unselect ->*/}
-    
-      {/* edit this to make the scroll vertical And Flex:1 , so you can see all the items. */}
-      <ScrollView style={{ padding: 2, marginStart: -45, flex: 1 }}>
-  {
-   
-    <TouchableOpacity style={{
-      flexDirection: "row",
-      flexWrap:"wrap",
-      // alignItems: "center",
-      justifyContent: "space-between",
-      marginTop: 10,
-    }}>
-      {itemData.map((item, index) => (
-        <ProductCard item={item} key={index} />
-      ))}
-    </TouchableOpacity>
-  }
-</ScrollView>
+      <View style={styles.containerCategories}>
+        <FlatList
+          data={cat}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.CategoryName}
+          horizontal
+        />
+      </View>
 
+      <ScrollView style={{ padding: 2, marginStart: -45, flex: 1 }}>
+        {filteredData.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => OnProductPress(item.id)} // Use item.id or another unique identifier
+            style={{
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+              marginTop: 10,
+            }}
+          >
+            <ProductCard item={item} />
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 };
